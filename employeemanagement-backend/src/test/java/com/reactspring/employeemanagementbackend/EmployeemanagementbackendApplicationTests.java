5b149c160647d24ec6955be892d409c937318f70
LyndaLabranche <lynda.labranche@gmail.com>
2023-12-30 22:45:51 -0800

    backend done, frontend started

---
+++ employeemanagement-backend/src/test/java/com/reactspring/employeemanagementbackend/EmployeemanagementbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactspring.employeemanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
